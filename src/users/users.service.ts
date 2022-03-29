164f433ea452464f40b625a09bd7db080d372a10
AlexeyYakovlev1 <alexeyykovlev555@gmail.com>
2022-03-29 20:25:18 +0300

    roles guard, roles decorator

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,7 +11,7 @@ export class UsersService {
 	
 	async createUser(dto:CreateUserDto) {
 		const user = await this.userRepository.create(dto);
-		const role = await this.rolesService.getRoleByValue("USER");
+		const role = await this.rolesService.getRoleByValue("ADMIN");
 
 		await user.$set("roles", [role.id]);
 		user.roles = [role];
